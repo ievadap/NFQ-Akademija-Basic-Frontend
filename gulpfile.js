bfab5f07633644b92c45318e3b01fe3fd57af3ab
Ieva Dapševičiūtė <i.dapseviciute@gmail.com>
2017-03-21 06:11:12 +0200

    created my cv

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('connect', function() {
     connect.server({
@@ -31,4 +33,16 @@ gulp.task('watch', function() {
     gulp.watch('./src/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['connect', 'sass', 'js', 'watch']);
+gulp.task('compress', function () {
+            gulp.src('./src/js/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('web/assets'));
+});
+
+gulp.task('minify-css', function () {
+    return gulp.src('./web/assets/*.css')
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./web/assets/min'))
+});
+
+gulp.task('default', ['connect', 'sass', 'js', 'watch', 'compress']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
-'use strict';
+//dropdown
+$(document).ready(function () {
+    $(".dropdown-toggle").dropdown();
+});
 
-alert('Labas!');
+//scrollspy
+$(document).ready(function () {
+    removeSection = function (e) {
+        $(e).parents("body > div").remove();
+        $('body').each(function () {
+            $(this).scrollspy('refresh');
+        })
+    };
+    $("body").scrollspy({target: '#nav-bar'});
+});
